047893dd2288966f7693f35e27a5cea014cb4f12
wanxinjin <wanxinjin@gmail.com>
2022-02-08 16:04:31 -0500

    update

--- cartpole/cartpole_class.py
+++ cartpole/cartpole_class.py
@@ -4,9 +4,10 @@ import numpy.linalg as la
 import matplotlib.pyplot as plt
 
 
-class LCS_learner:
+class cartpole_learner:
     def __init__(self, n_state, n_control, n_lam,
-                 A=None, B=None, C=None, D=None, E=None, G=None, lcp_offset=None, stiffness=0.):
+                 A=None, B=None, C=None, D=None, E=None, G=None, H=None, lcp_offset=None,
+                 stiffness=0.):
         self.n_lam = n_lam
         self.n_state = n_state
         self.n_control = n_control
@@ -16,24 +17,6 @@ class LCS_learner:
         self.u = SX.sym('u', self.n_control)
 
         self.theta = []
-
-        if G is None:
-            self.G = SX.sym('G', self.n_lam, self.n_lam)
-            self.theta += [vec(self.G)]
-        else:
-            self.G = DM(G)
-
-        if E is None:
-            self.E = SX.sym('E', self.n_lam, self.n_control)
-            self.theta += [vec(self.E)]
-        else:
-            self.E = DM(E)
-
-        if D is None:
-            self.D = SX.sym('D', self.n_lam, self.n_state)
-            self.theta += [vec(self.D)]
-        else:
-            self.D = DM(D)
 
         if lcp_offset is None:
             self.lcp_offset = SX.sym('lcp_offset', self.n_lam)
@@ -59,10 +42,34 @@ class LCS_learner:
         else:
             self.C = DM(C)
 
+        if D is None:
+            self.D = SX.sym('D', self.n_lam, self.n_state)
+            self.theta += [vec(self.D)]
+        else:
+            self.D = DM(D)
+
+        if E is None:
+            self.E = SX.sym('E', self.n_lam, self.n_control)
+            self.theta += [vec(self.E)]
+        else:
+            self.E = DM(E)
+
+        if G is None:
+            self.G = SX.sym('G', self.n_lam, self.n_lam)
+            self.theta += [vec(self.G)]
+        else:
+            self.G = DM(G)
+
+        if H is None:
+            self.H = SX.sym('H', self.n_lam, self.n_lam)
+            self.theta += [vec(self.H)]
+        else:
+            self.H = DM(H)
+
         self.theta = vcat(self.theta)
         self.n_theta = self.theta.numel()
 
-        self.F = stiffness * np.eye(self.n_lam) + self.G @ self.G.T
+        self.F = stiffness * np.eye(self.n_lam) + self.G @ self.G.T + self.H - self.H.T
         self.F_fn = Function('F_fn', [self.theta], [self.F])
         self.D_fn = Function('D_fn', [self.theta], [self.D])
         self.E_fn = Function('E_fn', [self.theta], [self.E])
@@ -72,25 +79,28 @@ class LCS_learner:
         self.C_fn = Function('C_fn', [self.theta], [self.C])
         self.lcp_offset_fn = Function('lcp_offset_fn', [self.theta], [self.lcp_offset])
 
-    def differetiable(self, gamma=1e-4, epsilon=1e0):
+    def differetiable(self, gamma=1e0, epsilon=1e1):
 
         # define the dynamics loss
         self.x_next = SX.sym('x_next', self.n_state)
-        data = vertcat(self.x, self.x_next)
-        dyn = self.A @ self.x + self.C @ self.lam
-        dyn_loss = dot(dyn - self.x_next, dyn - self.x_next)
+        data = vertcat(self.x, self.u, self.x_next)
+        self.dyn = self.A @ self.x + self.B @ self.u + self.C @ self.lam
+        dyn_loss = dot(self.dyn - self.x_next,self.dyn - self.x_next)
 
         # lcp loss
-        dist = self.D @ self.x + self.F @ self.lam + self.lcp_offset
+        self.dist = self.D @ self.x + self.E @ self.u + self.F @ self.lam + self.lcp_offset
         self.phi = SX.sym('phi', self.n_lam)
-        lcp_loss = dot(self.lam, self.phi) + 1 / gamma * dot(self.phi - dist, self.phi - dist)
+        lcp_loss = dot(self.lam, self.phi) + 1 / gamma * dot(self.phi - self.dist,
+                                                             self.phi - self.dist)
 
         # total loss
         loss = dyn_loss + lcp_loss / epsilon
+        # loss = dot(self.dyn[2:4] - self.x_next[2:4], self.dyn[2:4] - self.x_next[2:4]) + lcp_loss / epsilon
+        # loss = (dyn_loss + lcp_loss / epsilon) / (0.5+dot(self.x_next, self.x_next))
 
         # establish the qp solver
         lam_phi = vertcat(self.lam, self.phi)
-        data_theta = vertcat(self.x, self.x_next, self.theta)
+        data_theta = vertcat(self.x, self.u, self.x_next, self.theta)
         quadprog = {'x': vertcat(self.lam, self.phi), 'f': loss, 'p': data_theta}
         opts = {'printLevel': 'none', }
         self.inner_QPSolver = qpsol('inner_QPSolver', 'qpoases', quadprog, opts)
@@ -102,28 +112,14 @@ class LCS_learner:
         self.lcp_loss_fn = Function('lcp_loss_fn', [data, self.theta, lam_phi], [lcp_loss])
 
         # compute the second order derivative
-        grad_loss = jacobian(loss, lam_phi).T
-        L = diag(lam_phi) @ grad_loss
-        self.L_fn = Function('L_fn', [data, self.theta, lam_phi], [L])  # this is just for testing
-        # compute the gradient of lam_phi_opt with respect to theta
-        dL_dsol = jacobian(L, lam_phi)
-        dL_dtheta = jacobian(L, self.theta)
-        dsol_dtheta = -inv(dL_dsol) @ dL_dtheta
-        self.dsol_dtheta_fn = Function('dsol_dtheta_fn', [data, self.theta, lam_phi], [dsol_dtheta])
-        # this is just for testing
-        dloss2 = jacobian(loss, self.theta) + jacobian(loss, lam_phi) @ dsol_dtheta
-        self.dloss2_fn = Function('dloss2_fn', [data, self.theta, lam_phi], [dloss2.T])
-        # compute the second order derivative
-        dloss_dtheta = jacobian(loss, self.theta).T
-        ddloss = jacobian(dloss_dtheta, self.theta) + jacobian(dloss_dtheta, lam_phi) @ dsol_dtheta
-        self.ddloss_fn = Function('ddloss_fn', [data, self.theta, lam_phi], [ddloss])
+        # TBD
 
-    def compute_lambda(self, x_batch, x_next_batch, theta_val):
+    def compute_lambda(self, x_batch, u_batch, x_next_batch, theta_val):
         self.differetiable()
 
         # prepare the data
         batch_size = x_batch.shape[0]
-        data_batch = np.hstack((x_batch, x_next_batch))
+        data_batch = np.hstack((x_batch, u_batch, x_next_batch))
         theta_val_batch = np.tile(theta_val, (batch_size, 1))
         data_theta_batch = np.hstack((data_batch, theta_val_batch))
 
@@ -134,9 +130,9 @@ class LCS_learner:
 
         return lam_phi_opt_batch, loss_opt_batch
 
-    def gradient_step(self, x_batch, x_next_batch, theta_val, lam_phi_opt_batch, second_order=False):
+    def gradient_step(self, x_batch, u_batch, x_next_batch, theta_val, lam_phi_opt_batch, second_order=False):
         batch_size = x_batch.shape[0]
-        data_batch = np.hstack((x_batch, x_next_batch))
+        data_batch = np.hstack((x_batch, u_batch, x_next_batch))
         theta_val_batch = np.tile(theta_val, (batch_size, 1))
 
         # compute the gradient value
@@ -151,53 +147,87 @@ class LCS_learner:
         dyn_loss_mean = dyn_loss_batch.full().mean()
         lcp_loss_mean = lcp_loss_batch.full().mean()
 
-        # this is for testing
-        # testing = self.L_fn(data_batch.T, theta_val_batch.T, lam_phi_opt_batch.T).full()
-        # dtheta_batch2 = self.dloss2_fn(data_batch.T, theta_val_batch.T, lam_phi_opt_batch.T)
-        # dtheta_mean2 = dtheta_batch2.full().mean(axis=1)
-        # print(norm_2(dtheta_mean-dtheta_mean2))
-        dtheta_hessian = dtheta_mean
+        return dtheta_mean, loss_mean, dyn_loss_mean, lcp_loss_mean
 
-        if second_order is True:
-            hessian_batch = self.ddloss_fn(data_batch.T, theta_val_batch.T, lam_phi_opt_batch.T)
-            # compute the mean hessian
-            hessian_sum = 0
-            for i in range(batch_size):
-                hessian_i = hessian_batch[:, i * self.n_theta:(i + 1) * self.n_theta]
-                hessian_sum += hessian_i
-            hessian_mean = hessian_sum / batch_size
-            damping_factor = 1
-            u, s, vh = np.linalg.svd(hessian_mean)
-            s = s + damping_factor
-            damped_hessian = u @ np.diag(s) @ vh
-            dtheta_hessian = (inv(damped_hessian) @ DM(dtheta_mean)).full().flatten()
-
-        return dtheta_mean, loss_mean, dyn_loss_mean, lcp_loss_mean, dtheta_hessian
-
-    def dyn_prediction(self, x_batch, theta_val):
+    def dyn_prediction(self, x_batch, u_batch, theta_val):
         self.differetiable()
 
         batch_size = x_batch.shape[0]
         theta_val_batch = np.tile(theta_val, (batch_size, 1))
-        x_theta_batch = np.hstack((x_batch, theta_val_batch))
+        xu_theta_batch = np.hstack((x_batch, u_batch, theta_val_batch))
 
         # establish the lcp solver
-        dist = self.D @ self.x + self.F @ self.lam
-        lcp_loss = dot(dist, self.lam)
-        x_theta = vertcat(self.x, self.theta)
-        quadprog = {'x': self.lam, 'f': lcp_loss, 'g': dist, 'p': x_theta}
+        lcp_loss = dot(self.dist, self.lam)
+        xu_theta = vertcat(self.x, self.u, self.theta)
+        quadprog = {'x': self.lam, 'f': lcp_loss, 'g': self.dist, 'p': xu_theta}
         opts = {'printLevel': 'none'}
         lcp_Solver = qpsol('lcp_solver', 'qpoases', quadprog, opts)
+        self.lcp_fn = Function('dist_fn', [self.x, self.u, self.lam, self.theta], [self.dist, dot(self.dist, self.lam)])
+        self.lcp_dist_fn = Function('dist_fn', [self.x, self.u, self.lam, self.theta], [self.dist])
 
         # establish the dynamics equation
-        dyn = self.A @ self.x + self.C @ self.lam
-        dyn_fn = Function('dyn_fn', [self.x, self.lam, self.theta], [dyn])
+        dyn_fn = Function('dyn_fn', [self.x, self.u, self.lam, self.theta], [self.dyn])
 
         # compute the lam_batch
-        sol_batch = lcp_Solver(lbx=0., lbg=0., p=x_theta_batch.T)
+        sol_batch = lcp_Solver(lbx=0., lbg=0., p=xu_theta_batch.T)
         lam_opt_batch = sol_batch['x'].full().T
 
         # compute the next state batch
-        x_next_batch = dyn_fn(x_batch.T, lam_opt_batch.T, theta_val_batch.T).full().T
+        x_next_batch = dyn_fn(x_batch.T, u_batch.T, lam_opt_batch.T, theta_val_batch.T).full().T
 
         return x_next_batch, lam_opt_batch
+
+
+# do statistics for the modes
+def statiModes(lam_batch, tol=1e-5):
+    # dimension of the lambda
+    n_lam = lam_batch.shape[1]
+    # total number of modes
+    total_n_mode = float(2 ** n_lam)
+
+    # do the statistics for the modes
+    lam_batch_mode = np.where(lam_batch < tol, 0, 1)
+    unique_mode_list, mode_count_list = np.unique(lam_batch_mode, axis=0, return_counts=True)
+    mode_frequency_list = mode_count_list / lam_batch.shape[0]
+
+    return unique_mode_list, mode_frequency_list
+
+
+# compute the boundaries for each mode
+# F \lambda +D x+lcp_offset
+# output a polytope for such specified mode: Ax+b>=0
+def mode_polytope(F, D, lcp_offset, mode_vector):
+    # detect the active mode index
+    tol = 1e-6
+    active_index = []
+    inactive_index = []
+    for i in range(mode_vector.size):
+        if mode_vector[i] < tol:
+            inactive_index += [i]
+        else:
+            active_index += [i]
+
+    # partition the matrix of F and matrix D into active parts and inactive parts
+    F11 = F[active_index][:, active_index]
+    F12 = F[active_index][:, inactive_index]
+    F21 = F[inactive_index][:, active_index]
+    F22 = F[inactive_index][:, inactive_index]
+    D1 = D[active_index]
+    D2 = D[inactive_index]
+    lcp_offset_1 = lcp_offset[active_index]
+    lcp_offset_2 = lcp_offset[inactive_index]
+
+    # output
+    A = D2 - F21 @ la.inv(F11) @ D1
+    b = lcp_offset_2 - F21 @ la.inv(F11) @ lcp_offset_1
+
+    return A, b
+
+
+# do the plot of differnet mode
+def plotModes(lam_batch, tol=1e-5):
+    # do the statistics for the modes
+    lam_batch_mode = np.where(lam_batch < tol, 0, 1)
+    unique_mode_list, mode_indices = np.unique(lam_batch_mode, axis=0, return_inverse=True)
+
+    return unique_mode_list, mode_indices
---
+++ cartpole/test.py
@@ -0,0 +1,199 @@
+import cartpole_class
+import lcs.optim as opt
+import numpy.linalg as la
+from casadi import *
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+
+def print(*args):
+    __builtins__.print(*("%.5f" % a if isinstance(a, float) else a
+                         for a in args))
+
+
+# color list
+color_list = np.linspace(0, 1, 10)
+
+# ==============================   load the generated LCS system   ==================================
+lcs_mats = np.load('cartpole_lcs.npy', allow_pickle=True).item()
+n_state = lcs_mats['n_state']
+n_control = lcs_mats['n_control']
+n_lam = lcs_mats['n_lam']
+A = lcs_mats['A']
+B = lcs_mats['B']
+C = lcs_mats['C']
+D = lcs_mats['D']
+E = lcs_mats['E']
+G = lcs_mats['G']
+H = lcs_mats['H']
+F = lcs_mats['F']
+lcp_offset = lcs_mats['lcp_offset']
+
+# ==============================# re-show the training plot   ==================================
+learned_res = np.load('learned.npy', allow_pickle=True).item()
+learned_theta = learned_res['theta_trace'][-1]
+print('####################training results analysis#########################')
+print('mode count for training data:', learned_res['train_mode_count'])
+print('mode list for training data:\n', learned_res['train_mode_list'])
+print('mode frequency for training data:\n', learned_res['train_mode_frequency'])
+print('mode count for prediction data:', learned_res['pred_mode_count'])
+print('mode list for prediction data:\n', learned_res['pred_mode_list'])
+print('mode frequency for prediction data:\n', learned_res['pred_mode_frequency'])
+print('prediction accuracy for each mode:\n', learned_res['pred_error_per_mode_list'])
+print('overall relative prediction error: \n', learned_res['relative_error'])
+print('---------------------------------------------------------------------------')
+
+# plot the true one
+fig0, ax = plt.subplots()
+train_x = learned_res['train_x']
+train_y = learned_res['train_y']
+train_mode_indices = learned_res['train_mode_indices']
+learned_y = learned_res['pred_y']
+learned_mode_indices = learned_res['pred_mode_indices']
+ax.set_title('Learned modes marked in (^), \n True modes marked in (o)')
+plt.scatter(train_x, train_y, c=color_list[train_mode_indices], s=80, alpha=0.3)
+# plot the predicted one
+ax.scatter(train_x, learned_y, c=color_list[learned_mode_indices], s=40, marker="^")
+plt.show()
+
+# ==============================   generate the testing data    ========================================
+# ！！！！！！！！！！！！！！！！make sure matching with line 27-33 in the train.py
+data_generator = cartpole_class.cartpole_learner(n_state, n_control, n_lam,
+                                                 A, B, C, D, E, G, H, lcp_offset, stiffness=0)
+# generate the testing data
+test_data_size = 1000
+test_x_batch = 0.1 * np.random.uniform(-1, 1, size=(test_data_size, n_state))
+test_u_batch = 0.1 * np.random.uniform(-1, 1, size=(test_data_size, n_control))
+test_x_next_batch, test_lam_opt_batch = data_generator.dyn_prediction(test_x_batch, test_u_batch, theta_val=[])
+# check the mode index
+test_mode_list, test_mode_frequency_list = cartpole_class.statiModes(test_lam_opt_batch)
+test_mode_list, test_mode_indices = cartpole_class.plotModes(test_lam_opt_batch)
+
+# ==============================   create the learner object    ========================================
+# ！！！！！！！！！！！！！！！！ make sure matching with line 60-63 in the train.py
+learner = cartpole_class.cartpole_learner(n_state, n_control, n_lam=n_lam, stiffness=10)
+
+# ================================   do some anlaysis for the prediction    ======================================
+pred_x_next_batch, pred_lam_opt_batch = learner.dyn_prediction(test_x_batch, test_u_batch, learned_theta)
+# compute the overall relative prediction error
+error_x_next_batch = pred_x_next_batch - test_x_next_batch
+relative_error = (la.norm(error_x_next_batch, axis=1) / la.norm(test_x_next_batch, axis=1)).mean()
+# compute the predicted mode statistics
+pred_mode_list0, pred_mode_frequency_list = cartpole_class.statiModes(pred_lam_opt_batch)
+pred_mode_list1, pred_mode_indices = cartpole_class.plotModes(pred_lam_opt_batch)
+pred_error_per_mode_list = []
+for i in range(len(pred_mode_list0)):
+    mode_i_index = np.where(pred_mode_indices == i)
+    mode_i_error = error_x_next_batch[mode_i_index]
+    mode_i_relative_error = (la.norm(mode_i_error, axis=1) / la.norm(test_x_next_batch[mode_i_index], axis=1)).mean()
+    pred_error_per_mode_list += [mode_i_relative_error]
+
+print('#################### testing results analysis#########################')
+print('mode count for testing data:', test_mode_frequency_list.size)
+print('mode list for testing data:\n', test_mode_list)
+print('mode frequency for testing data:\n', test_mode_frequency_list)
+print('mode count for prediction data:', pred_mode_frequency_list.size)
+print('mode list for prediction data:\n', pred_mode_list0)
+print('mode frequency for prediction data:\n', pred_mode_frequency_list)
+print('prediction accuracy for each mode:\n', pred_error_per_mode_list)
+print('---------------------------------------------------------------------------')
+
+# =============== plot the training data, each color for each mode  ======================================
+plot_x_indx = 3
+plot_y_indx = 3
+
+# plot the true one
+fig1, ax = plt.subplots()
+ax.set_title('True modes marked in (o)')
+test_x = test_x_batch[:, plot_x_indx]
+test_y = test_x_next_batch[:, plot_y_indx]
+plt.scatter(test_x, test_y, c=color_list[test_mode_indices], s=80, alpha=1)
+
+# plot the true one
+fig2, ax = plt.subplots()
+ax.set_title('Learned modes marked in (^), \n True modes marked in (o)')
+test_x = test_x_batch[:, plot_x_indx]
+test_y = test_x_next_batch[:, plot_y_indx]
+plt.scatter(test_x, test_y, c=color_list[test_mode_indices], s=80, alpha=0.3)
+# plot the predicted one
+pred_y = pred_x_next_batch[:, plot_y_indx]
+ax.scatter(test_x, pred_y, c=color_list[pred_mode_indices], s=40, marker="^")
+# plt.show()
+
+# plot the true one
+fig3, ax = plt.subplots()
+ax.set_title('Learned modes marked in (^)')
+test_x = test_x_batch[:, plot_x_indx]
+test_y = test_x_next_batch[:, plot_y_indx]
+plt.scatter(test_x, test_y, c=color_list[test_mode_indices], s=0, alpha=0.3)
+# plot the predicted one
+pred_y = pred_x_next_batch[:, plot_y_indx]
+ax.scatter(test_x, pred_y, c=color_list[pred_mode_indices], s=40, marker="^")
+# plt.show()
+
+
+# ==================== print some key results  =======================
+
+print('------------------------------------------------')
+error_x_next_batch = pred_x_next_batch - test_x_next_batch
+relative_error = (la.norm(error_x_next_batch, axis=1) / la.norm(test_x_next_batch, axis=1)).mean()
+print('relative prediction error:', relative_error)
+
+if False:
+    print('------------------------------------------------')
+    print('A')
+    print('true')
+    print(A)
+    print('learned')
+    print(learner.A_fn(learned_theta))
+    print('------------------------------------------------')
+    print('C')
+    print('true')
+    print(C)
+    print('learned')
+    print(learner.C_fn(learned_theta))
+    print('------------------------------------------------')
+    print('D')
+    print('true')
+    print(D)
+    print(learner.D_fn(learned_theta))
+    print('------------------------------------------------')
+    print('G')
+    print('true')
+    print(G)
+    print('learned')
+    print(learner.G_fn(learned_theta))
+    print('------------------------------------------------')
+    print('lcp_offset')
+    print(lcp_offset)
+    print('learned')
+    print(learner.lcp_offset_fn(learned_theta))
+
+print('------------------------------------------------')
+print('pred_x/true_x')
+print(pred_x_next_batch[0:10] / test_x_next_batch[0:10])
+print('prediction')
+print(pred_x_next_batch[0:10])
+print('true')
+print(test_x_next_batch[0:10])
+print('max:', np.amax(pred_x_next_batch[0:10] / test_x_next_batch[0:10]))
+print('min:', np.amin(pred_x_next_batch[0:10] / test_x_next_batch[0:10]))
+
+print('------------------------------------------------')
+np.set_printoptions(suppress=True)
+print('pred_lam')
+print(np.where(pred_lam_opt_batch[0:10] < 1e-5, 0, 1))
+print('test_lam')
+print(np.where(test_lam_opt_batch[0:10] < 1e-5, 0, 1))
+
+# # print('------------------------------------------------')
+# print('compute the distance')
+# pred_dist, pred_lcp_loss = learner.lcp_fn(test_x_batch.T, pred_lam_opt_batch.T, learned_theta)
+# pred_dist = pred_dist.full().T
+# pred_lcp_loss = pred_lcp_loss.full().T
+# print(pred_dist[0:10])
+# print('prod  the distance with lam')
+# print(pred_dist[0:10] * pred_lam_opt_batch[0:10])
+
+
+plt.show()
